cde0bf93da001589dd1d3dc25a58465dd91c0e28
Ana Beatriz Trindade <108698090+anabeatrizdmt@users.noreply.github.com>
2022-08-24 17:06:51 -0300

    templates

--- templates.js
+++ templates.js
@@ -5,3 +5,9 @@ const bebidaMenor = "suco";
 
 const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`;
 console.log(pedido);
+
+const num = 10;
+
+if (!num) {
+ console.log(`o número é ${num}`);
+}
